04df986a80273bee607ad86854a0795b8a979a18
Tiffany Huang <huangtt@usc.edu>
2016-10-22 00:45:13 -0700

    create separate function for done button click

--- app/src/main/java/edu/usc/parknpay/owner/AddSpotActivity.java
+++ app/src/main/java/edu/usc/parknpay/owner/AddSpotActivity.java
@@ -3,7 +3,9 @@ package edu.usc.parknpay.owner;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -12,12 +14,23 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import edu.usc.parknpay.R;
 import edu.usc.parknpay.TemplateActivity;
+import edu.usc.parknpay.authentication.RegistrationActivity;
+import edu.usc.parknpay.authentication.SetDefaultModeActivity;
+import edu.usc.parknpay.database.User;
 
 /**
  * Created by Bobo on 10/19/2016.
@@ -109,6 +122,31 @@ public class AddSpotActivity extends TemplateActivity {
         cancel.setAdapter(cancelAdapter);
     }
 
+    public void addSpot(View view) {
+        // If registration is successful, proceed to owner/seeker selection view.
+        // intent = new Intent(this, SetDefaultModeActivity.class);
+
+        // Ref.child("Users").child(userId).setValue(user);
+
+        //grabbing all the values from the inputs (if all inputs are valid)
+        String notesFinal = notes.getText().toString();
+        String sizeFinal = size.getSelectedItem().toString();
+        String cancelFinal = cancel.getSelectedItem().toString();
+        boolean handicappedFinal = handicapped.isChecked();
+
+        // If registration is successful, proceed to owner/seeker selection view.
+        // intent = new Intent(this, SetDefaultModeActivity.class);
+
+        // Ref.child("Users").child(userId).setValue(user);
+
+        //should be sending to database here
+
+
+        Intent intent = new Intent(getApplicationContext(), OwnerMainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        startActivity(intent);
+    }
+
     protected void addListeners() {
         parkingSpotPhoto.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v)
@@ -116,29 +154,6 @@ public class AddSpotActivity extends TemplateActivity {
                 Intent pickPhoto = new Intent(Intent.ACTION_PICK,
                         android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(pickPhoto , 1);//one can be replaced with any action code
-            }
-
-        });
-
-
-        doneButton.setOnClickListener(new View.OnClickListener() {
-            //Submitting all the data that user entered for parking spot here.
-            @Override
-            public void onClick(View arg0) {
-                //grabbing all the values from the inputs (if all inputs are valid)
-                String streetFinal = street.getText().toString();
-                String cityFinal = city.getText().toString();
-                String stateFinal = state.getText().toString();
-                String zipFinal = zipCode.getText().toString();
-                String notesFinal = notes.getText().toString();
-                String sizeFinal = size.getSelectedItem().toString();
-                String cancelFinal = cancel.getSelectedItem().toString();
-                boolean handicappedFinal = handicapped.isChecked();
-                //should be sending to database here
-                Intent intent = new Intent(getApplicationContext(), OwnerMainActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-                startActivity(intent);
-
             }
 
         });
