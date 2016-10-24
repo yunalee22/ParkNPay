b4302b4eeeacf2819191ccc42e48ebb0a7275871
Timothy Hwang <uscitphwangtim@gmail.com>
2016-10-23 17:03:05 -0700

    geotagging

--- app/src/main/java/edu/usc/parknpay/LoginActivity.java
+++ app/src/main/java/edu/usc/parknpay/LoginActivity.java
@@ -1,6 +1,8 @@
 package edu.usc.parknpay;
 
 
+        import android.location.Address;
+        import android.location.Geocoder;
         import android.os.Bundle;
         import android.support.v7.app.AppCompatActivity;
         import android.support.v7.widget.Toolbar;
@@ -16,6 +18,8 @@ package edu.usc.parknpay;
         import com.google.android.gms.location.places.ui.PlaceSelectionListener;
         import com.google.android.gms.maps.model.LatLng;
         import com.google.android.gms.maps.model.LatLngBounds;
+
+        import java.util.List;
 
 public class LoginActivity extends AppCompatActivity implements PlaceSelectionListener {
 
@@ -37,13 +41,40 @@ public class LoginActivity extends AppCompatActivity implements PlaceSelectionLi
                 getFragmentManager().findFragmentById(R.id.place_fragment);
         autocompleteFragment.setOnPlaceSelectedListener(this);
         autocompleteFragment.setHint("Search a Location");
-        autocompleteFragment.setBoundsBias(BOUNDS_MOUNTAIN_VIEW);
+        autocompleteFragment.setBoundsBias(new LatLngBounds(
+                new LatLng(34.0224, -118.2851),
+                new LatLng(34.0224, -118.2851)));
+
+        //new LatLng(34.0224, 118.2851),
+        //new LatLng(-33.880490, 151.184363),
+        //new LatLng(-33.858754, 151.229596))
 
     }
 
     @Override
     public void onPlaceSelected(Place place) {
         Log.i(LOG_TAG, "Place Selected: " + place.getName());
+
+        //Toast.makeText(this, "TIM: "+ place.getName(), Toast.LENGTH_LONG).show();
+
+        Geocoder coder = new Geocoder(this);
+        List<Address> address;
+
+        String str = place.getName().toString();
+
+        try {
+            address = coder.getFromLocationName(str, 5);
+            Address location=address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+            Log.i(LOG_TAG, "SHOW LAT: " + location.getLatitude());
+
+            Toast.makeText(this, "Latititude is "+location.getLatitude(), Toast.LENGTH_LONG);
+        } catch (Exception IOException) {
+            Toast.makeText(this, "IOException", Toast.LENGTH_LONG).show();
+        }
+
+
     }
 
     @Override
