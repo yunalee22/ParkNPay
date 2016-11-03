ef94334701db56e2dccb0268d2def8ad827bdb6c
Bobo Liu <hidremafro@throwam.com>
2016-11-03 16:45:22 -0700

    ui updates

--- app/src/main/java/edu/usc/parknpay/seeker/ViewSpotActivity.java
+++ app/src/main/java/edu/usc/parknpay/seeker/ViewSpotActivity.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -67,7 +68,6 @@ public class ViewSpotActivity extends TemplateActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.seeker_view_spot);
         super.onCreateDrawer();
-        toolbarSetup();
 
         // Get references to UI views
         parkingSpotImage = (ImageView) findViewById(R.id.parkingSpotImage);
@@ -82,6 +82,28 @@ public class ViewSpotActivity extends TemplateActivity{
         ownerRatingBar = (RatingBar) findViewById(R.id.ownerRatingBar);
         reserveButton = (Button) findViewById(R.id.reserveButton);
         reviewsListView = (ListView) findViewById(R.id.reviewsListView);
+        //This function allows for listviews within scrollviews to be scrolled
+        reviewsListView.setOnTouchListener(new ListView.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                switch (action) {
+                    case MotionEvent.ACTION_DOWN:
+                        // Disallow ScrollView to intercept touch events.
+                        v.getParent().requestDisallowInterceptTouchEvent(true);
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        // Allow ScrollView to intercept touch events.
+                        v.getParent().requestDisallowInterceptTouchEvent(false);
+                        break;
+                }
+
+                // Handle ListView touch events.
+                v.onTouchEvent(event);
+                return true;
+            }
+        });
 
         // Add adapter to ListView
         reviews = new ArrayList<Review>();
@@ -164,12 +186,6 @@ public class ViewSpotActivity extends TemplateActivity{
         reviews.add(t);
     }
 
-    protected void toolbarSetup() {
-        Toolbar mToolBar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(mToolBar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setTitle("View Spot");
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
